ec651083a34ead34776e050bc54c1231b1f46465
LeeX57 <166258247+LeeX57@users.noreply.github.com>
2024-04-09 03:17:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,14 +42,17 @@ def read_file_as_image(data) -> np.ndarray:
 async def predict(
     file: UploadFile = File(...)
 ):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
-    
-    predictions = MODEL.predict(img_batch)
+    try:
+        image = read_file_as_image(await file.read())
+        img_batch = np.expand_dims(image, 0)
+        
+        predictions = MODEL.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
-    return {
-        'class': predicted_class,
-        'confidence': float(confidence)
-    }
+        predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        return {
+            'class': predicted_class,
+            'confidence': float(confidence)
+        }
+    except Exception as e:
+        return {"error": str(e)}
